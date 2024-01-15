0ed5fdc0f9324a4f6093f04266240f7222ea1d28
burak-biyikli <burak.biyikli@yahoo.com>
2024-01-14 19:36:58 -0600

    Added starter code to dump.c and dump.h

--- Dump.c
+++ Dump.c
@@ -18,11 +18,17 @@
 #include "../inc/Dump.h"
 #include "../inc/tm4c123gh6pm.h"
 
-// Global variables
+// Global variables (Dump)
 uint32_t DumpTimeBuffer[DUMPBUFSIZE];
 uint32_t DumpDataBuf[DUMPBUFSIZE];
 uint32_t DumpNum;
 
+// Global variables (Jitter)
+uint32_t MinElapsed;
+uint32_t MaxElapsed;
+uint32_t LastCallTime;
+
+//Since some functions require the current time, we can setup a hardware timer to run
 void Timer1_Init(void) {
     volatile uint32_t delay;
     SYSCTL_RCGCTIMER_R    |= 0x02;                // 0) activate TIMER1
@@ -35,25 +41,36 @@ void Timer1_Init(void) {
     TIMER1_CTL_R           = 0x00000001;          // 10) enable TIMER1A
 }
 
+// What should dump init do?
+// - reset the dump memory so we don't see old data.
+// - reset or start any timing required for our DumpTimeBuffer.
 void DumpInit(void){
-    /* TODO (EE445L Lab 2): complete this. */
-    
-    // What should dump init do?
-    // - reset the dump memory so we don't see old data.
-    // - reset or start any timing required for our DumpTimeBuffer.
+
+    //Initalize global data structures before any useage occurs
+    for(int i = 0; i < DUMPBUFSIZE; i++){
+        DumpTimeBuffer[i] = 0;
+        DumpDataBuf[i] = 0;
+    }
+    DumpNum = 0;
+
+    //Start/Restart timer required for our DumpTimeBuffer.    
+    Timer1_Init();
 }
 
+// Use TIMER1_TAR_R as current time.
+// Note: how do you deal with timer overflow?
 void DumpCapture(uint32_t data){
-    /* TODO (EE445L Lab 2): complete this. */
-    
-    // Use TIMER1_TAR_R as current time.
-    // 
-    // Note: how do you deal with timer overflow?
+
+    // Only capture while there is space in the buffers, no overwriting
+    if(DumpNum < DUMPBUFSIZE){  
+        DumpDataBuf[DumpNum] = data;
+        DumpTimeBuffer[DumpNum] = TIMER1_TAR_R;
+        DumpNum += 1;
+    }
 }
 
 uint32_t DumpCount(void){ 
-    /* TODO (EE445L Lab 2): complete this. */
-    return 0;
+    return DumpNum;
 }
 
 uint32_t* DumpData(void){
@@ -64,30 +81,63 @@ uint32_t* DumpTime(void){
     return DumpTimeBuffer;
 }
 
-void JitterInit(void){
-    /* TODO (EE445L Lab 2): complete this. */
-    
-    // Note that jitter is independent of dump and should not 
-    // share the same data structures. Create and reset your own 
-    // variables indicating the measured jitter.
+// Note that jitter is independent of dump and should not 
+// share the same data structures. Create and reset your own 
+// variables indicating the measured jitter.
+void JitterInit(void){    
+    MinElapsed = 0xFFFFFFFF;
+    MaxElapsed = 0;
+    LastCallTime = 0;
+    Timer1_Init();      //Note that this reset of the timer can cause odd behavior in your data dump
 }
 
+// We'll define the jitter we want to measure as "peak-to-peak" 
+// jitter or "cycle-to-cycle" jitter; this is the maximum time 
+// difference between any two periodic measurements.
+//
+// We determine this by measuring the time of successive calls:
+// on the first time called, JitterMeasure just measures current 
+// time. On subsequent calls, JitterMeasure measures the elapsed time
+// from previous calls.
+// 
+// Note: how do you deal with timer overflow?
 void JitterMeasure(void){
-    /* TODO (EE445L Lab 2): complete this. */
     
-    // We'll define the jitter we want to measure as "peak-to-peak" 
-    // jitter or "cycle-to-cycle" jitter; this is the maximum time 
-    // difference between any two periodic measurements.
-    //
-    // We determine this by measuring the time of successive calls:
-    // on the first time called, JitterMeasure just measures current 
-    // time. On subsequent calls, JitterMeasure measures the elapsed time
-    // from previous calls.
-    // 
-    // Note: how do you deal with timer overflow?
+    //Record the the current time at start of call
+    uint32_t currTime = TIMER1_TAR_R;
+
+    if (MinElapsed == 0xFFFFFFFF){
+    
+        // Technically reduces range of allowable periods compated to using a boolean flag
+        MinElapsed = 0xFFFFFFFE;
+
+    } else {
+
+        uint32_t currentElapsed = 0;
+
+        if (currTime > LastCallTime){ 
+            // Overflow Case
+            currentElapsed = LastCallTime + (0xFFFFFFFF - currTime);
+        } else {    
+            // Normal Case
+            currentElapsed = LastCallTime - currTime;
+        }
+        
+        // checking if lower than min
+        if (currentElapsed < MinElapsed){
+            MinElapsed = currentElapsed;
+        }
+        //checking if higher than max
+        if (currentElapsed > MaxElapsed){
+            MaxElapsed = currentElapsed;
+        }
+        
+    }
+
+    // update the previous time
+    LastCallTime = currTime;
 }
 
 uint32_t JitterGet(void){
-    /* TODO (EE445L Lab 2): complete this. */
-    return 42;
+    return MaxElapsed - MinElapsed;
 }
